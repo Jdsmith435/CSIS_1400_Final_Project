e2910a5d495ba7ab0e7d7904a05295c72f63463e
Jeremiah Smith <59949075+Jdsmith435@users.noreply.github.com>
2020-04-23 23:19:09 -0600

    "Commited txt file"

---
+++ Flashcard_Project/ToTxtFile.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class ToTxtFile
+{
+    public static void main (String [] args)
+    {
+        
+    }
+    
+    public static void toTXT (String questions[], String answers[], int size)
+        {
+        try{
+        FileWriter totxt = new FileWriter("FlashCards.txt");
+        for(int i = 0; i < questions.length; i++)
+            {
+                totxt.write(String.format((i+1) + ": " + questions[i]));
+                totxt.write(System.lineSeparator());
+                totxt.write(String.format((i+1)  + ": " + answers[i]));
+                totxt.write(System.lineSeparator());
+            }
+        totxt.close();
+        }catch(IOException e) {
+            e.printStackTrace();
+            }   
+        
+     
+    }
+}
